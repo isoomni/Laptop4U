87c4fe29dc3563a01a48d7b25330e3adddfd8f6d
root <isoomni02@gmail.com>
2021-11-13 12:45:42 +0000

    20211113 commit

--- main/admin.py
+++ main/admin.py
@@ -3,6 +3,19 @@ from django.contrib import admin
 # Register your models here.
 from .models import Laptop, Question, Choice
 
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_contents']})
+    ]
+    inlines = [ChoiceInline]
+
+
 admin.site.register(Laptop)
-admin.site.register(Question)
-admin.site.register(Choice)
+admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice)
--- main/views.py
+++ main/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
 from .models import Question, Laptop, Choice
 
+
 def index(request):
     laptops = Laptop.objects.all()
-    
+
     context = {
         'laptops': laptops,
     }
-    
+
     return render(request, 'main/index.html', context=context)
